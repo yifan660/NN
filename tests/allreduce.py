464f84df00f239a607a38bdc7adc3629e9910113
yifan660 <56948499+yifan660@users.noreply.github.com>
2023-12-22 12:00:05 +0800

    Create allreduce.py

---
+++ tests/allreduce.py
@@ -0,0 +1,50 @@
+import os
+import torch
+import torch.distributed as dist
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from math import ceil
+from random import Random
+from torch.multiprocessing import Process
+from torch.autograd import Variable
+from torchvision import datasets, transforms
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1,10,kernel_size=5)
+        self.conv2 = nn.Conv2d(10,10,kernel_size=5)
+        self.fc_dropout = nn.Dropout2d()
+        self.fc1 = nn.Linear(100,50)
+        self.fc2 = nn.Linear(50,10)
+    
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x)
+
+
+class grad():
+    dist.all_reduce()
+
+
+def init_processes():
+
+def.run():
+    model()
+if __name__ == "__main__":
+    size = 2
+    processes = []
+    for rank in range(size):
+        p = Process(target=init_processes, args=(rank, size, run))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
