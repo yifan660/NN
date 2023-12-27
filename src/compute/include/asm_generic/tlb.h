a603330c0ddaf498ba6e3dfb9adfcdf7eedf0db1
yifan660 <56948499+yifan660@users.noreply.github.com>
2023-12-27 11:59:41 +0800

    Create tlb.h

---
+++ src/compute/include/asm_generic/tlb.h
@@ -0,0 +1,21 @@
+#ifndef 
+#define
+
+
+#include 
+#include
+#include
+#include
+#include
+#include
+
+
+#ifndef tlb_needs_table_invalidate
+#define
+
+#define
+#define
+#define
+#define
+#define
+static
