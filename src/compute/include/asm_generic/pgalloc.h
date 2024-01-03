549fb94a79afcd588b1048c17b620f23e6f268a2
yifan660 <56948499+yifan660@users.noreply.github.com>
2024-01-03 19:44:38 +0800

    Update pgalloc.h

--- src/compute/include/asm_generic/pgalloc.h
+++ src/compute/include/asm_generic/pgalloc.h
@@ -16,3 +16,51 @@ static inline pte_t *__pte_alloc_one_kernel(struct mm_struct *mm)   {
 
     return ptdesc_page(ptdesc);
 }
+
+
+static inline void pte_free(struct mm_struct *mm, struct page *pte_page)    {
+    struct ptdesc *ptdesc = page_ptdesc(pte_page);
+
+    pagetable_pte_dtor(ptdesc);
+    pagetable_free(ptdesc);
+}
+
+
+#if CONFIG_PGTABLE_LEVELS > 2
+
+#ifndef __HAVE_ARCH_PMD_ALLOC_ONE
+
+static inline pmd_t *pmd_alloc_one(struct mm_struct *mm, unsigned long addr)    {
+    struct ptdesc *ptdesc;
+    gfp_t gfp = GFP_PGTABLE_USER;
+
+    if(mm==&init_mm)    {
+        gfp = GFP_PGTABLE_KERNEL;
+    }
+    ptdesc = pagetable_alloc(gfp, 0);
+    if(!ptdesc)    {
+        return NULL;
+    }
+    if(!pagetable_pmd_ctor(ptdesc))    {
+        pagetable_free(ptdesc);
+        return NULL;
+    }
+    return ptdesc_address(ptdesc);
+}
+#endif
+
+
+#ifndef __HAVE_ARCH_PMD_FREE
+static inline void pmd_free(struct mm_struct *mm, pmd_t *pmd)   {
+    struct ptdesc *ptdesc = virt_to_ptdesc(pmd);
+
+    BUG_ON((unsigned long)pmd & (PAGE_SIZE-1));
+
+}
+
+
+static inline void __pud_free() {
+    
+}
+
+static inline
